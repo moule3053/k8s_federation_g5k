e85376ce962cc6dfa9b87ae56390ae53abfd073d
moule3053 <mulugetayalew@gmail.com>
2020-04-22 10:45:19 +0200

    Fixed some configurations

--- k8s_federation_g5k.py
+++ k8s_federation_g5k.py
@@ -1,5 +1,5 @@
 import subprocess
-from enoslib.api import discover_networks, play_on, generate_inventory,run_ansible,gather_facts
+from enoslib.api import discover_networks, generate_inventory,run_ansible
 from enoslib.infra.enos_vmong5k.provider import VMonG5k
 from enoslib.infra.enos_vmong5k.configuration import Configuration
 
@@ -8,13 +8,13 @@ import time
 
 name = "kubefed-"
 
-clusters = ["parapluie", "parapluie","econome", "chiclet", "dahu", "petitprince"]
+clusters = ["parapluie", "parapluie","ecotype", "chiclet"]
 
 logging.basicConfig(level=logging.DEBUG)
 
 master_nodes = []
 
-duration = "02:00:00"
+duration = "04:00:00"
 
 
 for i in range(0, len(clusters)):
@@ -28,6 +28,7 @@ for i in range(0, len(clusters)):
                                        image="/grid5000/virt-images/debian9-x64-base.qcow2")\
                         .add_machine(roles=[role_name],
                                      cluster=clusters[i],
+                                     flavour_desc={"core": 2, "mem": 4096},
                                      number=6)\
                         .finalize()
     provider = VMonG5k(conf)
@@ -43,14 +44,16 @@ for i in range(0, len(clusters)):
 
     # Make sure k8s is not already running
     run_ansible(["reset_k8s.yml"], inventory_path=inventory_file)
+    
     # Deploy k8s and dependencies
     run_ansible(["deploy_k8s_clusters.yml"], inventory_path=inventory_file)
 
-# Master nodes of all clusters
+# Master nodes of each cluster
 print("Master nodes ........")
 print(master_nodes)
 
 # Modify k8s conctext configurations to give them unique names
-subprocess.check_call("./modify_kube_config.sh %s %s %s %s %s %s " % (master_nodes[0]), str(master_nodes[1]), str(master_nodes[2]), str(master_nodes[3]), str(master_nodes[4]), str(master_nodes[5])), shell=True)
+subprocess.check_call("./modify_kube_config.sh %s %s %s %s" % (str(master_nodes[0]), str(master_nodes[1]), str(master_nodes[2]), str(master_nodes[3])), shell=True)
+
 # Setup Kubernetes Federation with Cluster 0 as the host cluster and the remaining clusters as members of the federation
 run_ansible(["kubefed_init.yml"], inventory_path="kubefed_inventory_cluster0.ini")
--- modify_kube_config.sh
+++ modify_kube_config.sh
@@ -3,10 +3,10 @@
 i=0
 for cluster in "$@"
 do
-scp -oStrictHostKeyChecking=no mulugeta@$cluster:~/.kube/config ~/.kube/cluster$i
+scp -oStrictHostKeyChecking=no root@$cluster:~/.kube/config ~/.kube/cluster$i
 sed -i 's/kubernetes-admin/k8s-admin-cluster'$i'/g' ~/.kube/cluster$i
 sed -i 's/name: kubernetes/name: cluster'$i'/g' ~/.kube/cluster$i
 sed -i 's/cluster: kubernetes/cluster: cluster'$i'/g' ~/.kube/cluster$i
-scp ~/.kube/cluster$i mulugeta@$1:~/.kube/
+scp ~/.kube/cluster$i root@$1:~/.kube/
 i=$((i+1))
 done
